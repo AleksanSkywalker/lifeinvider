c3709e520fec0e39db2a3a2888e2715d004780cb
AleksanSkywalker <it.karabatov@gmail.com>
2021-02-19 11:21:04 +0300

    21: 19-02-2021-worked! Dialogs.jsx

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
-const App = () => {
+const App = (props) => {
     return (
-        <div>
-            <Header/>
-            <Technologies />
-        </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Navbar/>
+                <Header/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/feeds' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
-const Header = () => {
-    return (
-        <div>
-            <a href='#s'>Home</a>
-            <a href='#s'>News Feed</a>
-            <a href='#s'>Messages</a>
-        </div>
-    );
-}
-
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>css</li>
-                <li>html</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    );
-}
-asdasdasd
 export default App;
