99b96d9a4ac9a92ddf253e783d414cd57bb799e8
AleksanSkywalker <it.karabatov@gmail.com>
2021-02-19 15:00:40 +0300

    26: 19-02-2021

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,6 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-
-    let posts = [
-        {id: 1, message: 'Hi, how are you?', like: 14},
-        {id: 2, message: 'I\'s my first post', like: 42},
-        {id: 3, message: 'Yo', like: 420},
-        {id: 3, message: 'Yo', like: 420},
-        {id: 3, message: 'Yo', like: 420},
-    ]
-
-    let postsElements = posts.map ( p => <Post message={p.message} like={p.like}/>);
-
     return (
         <div className={s.postsBlock}>
             <div>
@@ -25,9 +14,6 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <h2> My posts </h2>
-            <div className={s.posts}>
-                {postsElements}
-            </div>
         </div>
     );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import Post from "./MyPosts/Post/Post";
 
 const Profile = () => {
+
+    let posts = [
+        {id: 1, message: 'Hi, how are you?', like: 14},
+        {id: 2, message: 'I\'s my first post', like: 42},
+        {id: 3, message: 'Yo', like: 420},
+        {id: 3, message: 'Yo', like: 420},
+        {id: 3, message: 'Yo', like: 420},
+    ]
+
+    let postsElements = posts.map ( p => <Post message={p.message} like={p.like}/>);
+
     return (
         <div>
             <ProfileInfo/>
             <MyPosts/>
+            {postsElements}
         </div>
     );
 }
