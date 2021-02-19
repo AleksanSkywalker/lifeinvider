a243b243a855907b1049cce9aa849c579401a736
AleksanSkywalker <it.karabatov@gmail.com>
2021-02-19 16:06:51 +0300

    29: 19-02-2021 - worked! data to state.js

---
+++ src/redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi', like: 12},
+            {id: 2, message: 'Fuck you', like: 25},
+            {id: 3, message: 'Fine! )', like: 1},
+            {id: 4, message: 'Thanks', like: 42}
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Aleksan'},
+            {id: 2, name: 'Serge'},
+            {id: 3, name: 'Alex'},
+            {id: 4, name: 'Ruslan'},
+            {id: 5, name: 'Yuliia'},
+            {id: 6, name: 'Yayceslav'},
+            {id: 7, name: 'Marina'},
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Fuck you'},
+            {id: 3, message: 'Fine! )'},
+            {id: 4, message: 'Thanks'},
+        ]
+    },
+}
+export default state;
